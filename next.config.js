7819d6ae1c28a663255f9b071affe72414ef74b4
Ayush shah <aaketanbhai@gmail.com>
2026-04-18 22:53:09 +0530

    config fix

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
+const isProd = process.env.NODE_ENV === 'production';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
   trailingSlash: true,
+  basePath: isProd ? '/woodi' : '',
+  assetPrefix: isProd ? '/woodi/' : '',
   images: {
     unoptimized: true,
   },
